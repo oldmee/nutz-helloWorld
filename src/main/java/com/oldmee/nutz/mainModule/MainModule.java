88605eda9f84f343c0f711623e6adf75bf2bd991
R.oldmee <280468014@qq.com>
2019-01-20 15:46:24 +0800

    为正式开发做准备，配置动作链

--- src/main/java/com/oldmee/nutz/mainModule/MainModule.java
+++ src/main/java/com/oldmee/nutz/mainModule/MainModule.java
@@ -11,11 +11,14 @@ import org.nutz.mvc.ioc.provider.ComboIocProvider;
 //@Modules(scanPackage = true) //1.r.58开始默认就是true
 @SetupBy(value=MainSetup.class)
 @Modules(scanPackage=true)
-@IocBy(type= ComboIocProvider.class, args={"*js", "src/",
+@IocBy(type= ComboIocProvider.class, args={"*js", "ioc/",
         // 这个package下所有带@IocBean注解的类,都会登记上
         "*anno", "com.oldmee.nutz.mainModule", // *anno 是AnnotationIocLoader,负责处理注解式Ioc, 例如@IocBean
         "*tx", // 事务拦截 aop
         "*async"}) // 异步执行aop
+@Ok("json:full")
+@Fail("jsp:jsp.500")
+@Localization(value="msg/", defaultLocalizationKey="zh-CN")
 public class MainModule {
 
 }
